6c8326556ffce8c2384ef8b6f22147b082df1a82
Sjaitawat <soumya.jaitawat@gmail.com>
2022-04-21 20:14:25 +0530

    first

---
+++ main.py
@@ -0,0 +1,89 @@
+from typing import Optional
+from uuid import UUID, uuid4
+from pydantic import BaseModel, Field
+from typing import List
+from fastapi import FastAPI, Path, Query
+
+
+app = FastAPI()
+
+
+var =[ {
+    "page": 2,
+    "per_page": 6,
+    "total": 12,
+    "total_pages": 2,
+    "data": [
+        {
+            "id": 7,
+            "email": "michael.lawson@reqres.in",
+            "first_name": "Michael",
+            "last_name": "Lawson",
+            "avatar": "https://reqres.in/img/faces/7-image.jpg"
+        },
+        {
+            "id": 8,
+            "email": "lindsay.ferguson@reqres.in",
+            "first_name": "Lindsay",
+            "last_name": "Ferguson",
+            "avatar": "https://reqres.in/img/faces/8-image.jpg"
+        },
+        {
+            "id": 9,
+            "email": "tobias.funke@reqres.in",
+            "first_name": "Tobias",
+            "last_name": "Funke",
+            "avatar": "https://reqres.in/img/faces/9-image.jpg"
+        },
+        {
+            "id": 10,
+            "email": "byron.fields@reqres.in",
+            "first_name": "Byron",
+            "last_name": "Fields",
+            "avatar": "https://reqres.in/img/faces/10-image.jpg"
+        },
+        {
+            "id": 11,
+            "email": "george.edwards@reqres.in",
+            "first_name": "George",
+            "last_name": "Edwards",
+            "avatar": "https://reqres.in/img/faces/11-image.jpg"
+        },
+        {
+            "id": 12,
+            "email": "rachel.howell@reqres.in",
+            "first_name": "Rachel",
+            "last_name": "Howell",
+            "avatar": "https://reqres.in/img/faces/12-image.jpg"
+        }
+    ],
+    "support": {
+        "url": "https://reqres.in/#support-heading",
+        "text": "To keep ReqRes free, contributions towards server costs are appreciated!"
+    }
+}]
+
+@app.get("/")
+def home():
+    return {"Hello":"welcome"}
+
+
+@app.get("/query")
+def show(page_no:int = Query(None,description="Show page ",le=2,gt=0)):
+    for new in var:
+        if new["page"]== page_no:
+          return new
+    
+
+@app.get("/queryforuser")
+def id(id_value:int = Query(None,description="Show id"),le=12,gt=7):
+    for new in var:
+        for neww in new["data"]:
+          if neww["id"]==id_value:
+              return neww
+              
+
+    for new in var:
+        return new["support"]
+
+
